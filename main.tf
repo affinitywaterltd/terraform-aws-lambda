bf63d3f224aaeff17144ac04a00cf22c1008a16c
Ellis <tim.ellis@affinitywater.co.uk>
2018-10-08 15:03:38 +0100

    test monthly report python script

---
+++ main.tf
@@ -0,0 +1,23 @@
+### Monthly report
+
+
+
+resource "aws_lambda_function" "monthly_report" {
+    function_name    = "Monthly_AWS_Report"
+    filename         = "monthly_report.lambda"
+    role             = "${data.terraform_remote_state.core.lambda_reporting_role.arn}"
+    source_code_hash = "${base64sha256(file("monthly_report.lambda"))}"
+    handler          = "lambda_handler"
+    runtime          = "python2.7"
+
+    description      = "Lists all the cost centre/quadrant for EC2 and RDS - emails and uploads to S3"
+
+    memory_size      = 128
+    timeout          = 300
+
+  environment {
+    variables = {
+      foo = "bar"
+    }
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,22 @@
+# Account settings 
+
+variable "account" {}
+variable "environment" {}
+
+
+### IAM Roles
+
+data "terraform_remote_state" "core" {
+  backend = "atlas"
+
+  config {
+    name = "AffinityWater/${var.account}-core-${var.environment}"
+  }
+}
+
+
+
+### Email settings
+variable "ses_smtp_user" {}
+variable "ses_smtp_password" {}
+
