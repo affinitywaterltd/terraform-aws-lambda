b9d105d3f5e2d0ab938edf4c7d8e63fd3d167acf
Ellis <tim.ellis@affinitywater.co.uk>
2018-10-08 16:03:13 +0100

    Adjusted handler name

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "aws_lambda_function" "monthly_report" {
   #role             = "${data.terraform_remote_state.core.iam.lambda_reporting_role.arn}"
   role             = "arn:aws:iam::986618351900:role/Lambda_Reporting"
   source_code_hash = "${base64sha256(file("${path.module}/monthly_report.zip"))}"
-  handler          = "lambda_handler"
+  handler          = "monthly_report.lambda_handler"
   runtime          = "python2.7"
 
   description = "Lists all the cost centre/quadrant for EC2 and RDS - emails and uploads to S3"
