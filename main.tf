c0257b817e12f30120f590c92310828b55c83496
Ellis <tim.ellis@affinitywater.co.uk>
2018-10-08 15:19:38 +0100

    Trying to get info from module directly

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@
 resource "aws_lambda_function" "monthly_report" {
   function_name    = "Monthly_AWS_Report"
   filename         = "monthly_report.lambda"
-  role             = "${data.terraform_remote_state.core.lambda_reporting_role.arn}"
+  role             = "${data.terraform_remote_state.core.iam.lambda_reporting_role.arn}"
   source_code_hash = "${base64sha256(file("${path.module}/monthly_report.lambda"))}"
   handler          = "lambda_handler"
   runtime          = "python2.7"
