19ff785d7dd6a0611940a583fdd4a583c8634f6c
Haddrell <Glenn.Haddrell@affinitywater.co.uk>
2018-11-21 11:33:15 +0000

    Reshuffle Structure

---
+++ monthly_report/main.tf
@@ -0,0 +1,24 @@
+### Monthly report
+
+resource "aws_lambda_function" "monthly_report" {
+  function_name = "Monthly_AWS_Report"
+  filename      = "${path.module}/monthly_report.zip"
+
+  #role             = "${data.terraform_remote_state.core.iam.lambda_reporting_role.arn}"
+  role             = "arn:aws:iam::986618351900:role/Lambda_Reporting"
+  source_code_hash = "${base64sha256(file("${path.module}/monthly_report.zip"))}"
+  handler          = "monthly_report.lambda_handler"
+  runtime          = "python2.7"
+
+  description = "Lists all the cost centre/quadrant for EC2 and RDS - emails and uploads to S3"
+
+  memory_size = 128
+  timeout     = 300
+
+  environment {
+    variables = {
+      foo = "bar"
+    }
+  }
+}
+
