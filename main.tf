87d2cf78a3197d7dba5809674513dcbdeadf6672
Haddrell <Glenn.Haddrell@affinitywater.co.uk>
2018-11-26 11:57:11 +0000

    Test update

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ resource "aws_lambda_function" "monthly_report" {
   filename      = "${path.module}/monthly_report.zip"
 
   #role             = "${data.terraform_remote_state.core.lambda_reporting_role}"
-  role             = "arn:aws:iam::633033879498:role/Lambda_Reporting"
+  role             = "Lambda_Reporting"
   source_code_hash = "${base64sha256(file("${path.module}/monthly_report.zip"))}"
   handler          = "monthly_report.lambda_handler"
   runtime          = "python2.7"
