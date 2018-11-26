439c41d2bb7ddaeb17ec298ee52a46020c700ddc
Haddrell <Glenn.Haddrell@affinitywater.co.uk>
2018-11-26 12:34:06 +0000

    Changes module to statis IAM role reference

--- main.tf
+++ main.tf
@@ -4,8 +4,9 @@ resource "aws_lambda_function" "monthly_report" {
   function_name = "Monthly_AWS_Report"
   filename      = "${path.module}/monthly_report.zip"
 
-  role             = "${local.lambda_role}"
-  #role             = "Lambda_Reporting"
+  #Couldnt get working referencing the role from the module output, added by static name instead
+  #role             = "${data.terraform_remote_state.core.lambda_report_role}" 
+  role             = "Lambda_Reporting"
   source_code_hash = "${base64sha256(file("${path.module}/monthly_report.zip"))}"
   handler          = "monthly_report.lambda_handler"
   runtime          = "python2.7"
--- variables.tf
+++ variables.tf
@@ -12,7 +12,3 @@ data "terraform_remote_state" "core" {
     name = "AffinityWater/${var.account}-core-${var.environment}"
   }
 }
-
-locals {
-    lambda_role =   "${data.terraform_remote_state.core.lambda_report_role}"
-}
