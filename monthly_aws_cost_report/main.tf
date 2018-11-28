b91831671d6df17a89e71536d1b1c96c9adbf8ff
Haddrell <Glenn.Haddrell@affinitywater.co.uk>
2018-11-28 18:15:50 +0000

    Cleaned up

--- monthly_aws_cost_report/main.tf
+++ monthly_aws_cost_report/main.tf
@@ -13,7 +13,7 @@ resource "aws_lambda_function" "monthly_aws_cost_report" {
   handler          = "monthly_aws_cost_report.lambda_handler"
   runtime          = "python2.7"
 
-  description = "Lists all the cost centre/quadrant for EC2 and RDS - emails and uploads to S3 - Updated ${var.ses_smtp_user}"
+  description = "Lists all the cost centre/quadrant for EC2 and RDS - emails and uploads to S3"
 
   memory_size = 128
   timeout     = 300
