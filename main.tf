bc36816b85ad87dd857b7e1c51727dc81391688c
Haddrell <Glenn.Haddrell@affinitywater.co.uk>
2018-11-28 14:15:55 +0000

    Updated

--- main.tf
+++ main.tf
@@ -1,4 +1,6 @@
 # AWS Monthly Report
 module "monthly_report" {
   source      = "./monthly_report"
+  account     = "${var.account}"
+  environment = "${var.environment}"
 }
---
+++ monthly_report/variables.tf
@@ -0,0 +1,7 @@
+data "terraform_remote_state" "core" {
+  backend = "atlas"
+
+  config {
+    name = "AffinityWater/${var.account}-core-${var.environment}"
+  }
+}
