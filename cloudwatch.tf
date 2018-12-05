8d140ca6e5c6e27362790cb4d1ce64157a3ef345
Haddrell <Glenn.Haddrell@affinitywater.co.uk>
2018-12-05 09:37:51 +0000

    Update role arn for cloudwatch

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -2,5 +2,5 @@ resource "aws_cloudwatch_event_rule" "schedule_start_of_month" {
   name        = "schedule_start_of_month"
   description = "Runs at the start of each calendar month"
   schedule_expression = "cron(0 1 1 * ? *)"
-  role_arn = "arn:aws:iam::431686256879:role/ManualTest_Lambda"
+  role_arn = "${data.terraform_remote_state.core.invoke_lambda_role}"
 }
