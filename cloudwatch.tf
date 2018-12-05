4532a466f11aa10c204b60060149f2ef0fae5b24
Haddrell <Glenn.Haddrell@affinitywater.co.uk>
2018-12-05 09:48:09 +0000

    Test schedule

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -1,6 +1,6 @@
 resource "aws_cloudwatch_event_rule" "schedule_start_of_month" {
   name        = "schedule_start_of_month"
   description = "Runs at the start of each calendar month"
-  schedule_expression = "cron(0 1 1 * ? *)"
+  schedule_expression = "cron(50 9 5 * ? *)"
   role_arn = "${data.terraform_remote_state.core.invoke_lambda_role}"
 }
