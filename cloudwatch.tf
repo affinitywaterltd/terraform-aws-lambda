466ebd47b336bc1346efbc90ef694d175429a05d
Haddrell <Glenn.Haddrell@affinitywater.co.uk>
2018-12-05 13:38:27 +0000

    Updated tiime

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -1,5 +1,5 @@
 resource "aws_cloudwatch_event_rule" "schedule_start_of_month" {
   name        = "schedule_start_of_month"
   description = "Runs at the start of each calendar month"
-  schedule_expression = "cron(20 12 5 * ? *)"
+  schedule_expression = "cron(40 13 5 * ? *)"
 }
