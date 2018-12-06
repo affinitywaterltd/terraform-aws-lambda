5f1c4e75e57abcdafc25f1b8493565a2526f23ed
Haddrell <Glenn.Haddrell@affinitywater.co.uk>
2018-12-06 19:01:08 +0000

    rework tags

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -2,4 +2,5 @@ resource "aws_cloudwatch_event_rule" "schedule_start_of_month" {
   name        = "schedule_start_of_month"
   description = "Runs at the start of each calendar month"
   schedule_expression = "cron(20 14 5 * ? *)"
+  
 }
