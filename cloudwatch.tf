6289a0ee4149ce547cfa7ad7b4c2aafd901bc97c
Haddrell <Glenn.Haddrell@affinitywater.co.uk>
2019-01-07 10:31:19 +0000

    Corrected schedule from test time

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -1,6 +1,6 @@
 resource "aws_cloudwatch_event_rule" "schedule_start_of_month" {
   name        = "schedule_start_of_month"
   description = "Runs at the start of each calendar month"
-  schedule_expression = "cron(20 14 5 * ? *)"
+  schedule_expression = "cron(0 1 1 * ? *)"
   
 }
