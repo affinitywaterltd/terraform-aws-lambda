90f2cfd61d1c6132acc119c1ba16e53f131e9073
Haddrell <Glenn.Haddrell@affinitywater.co.uk>
2019-11-14 15:55:23 +0000

    Update display name for SNS

--- daily_rds_check_free_space/sns.tf
+++ daily_rds_check_free_space/sns.tf
@@ -1,7 +1,9 @@
 resource "aws_sns_topic" "sns_alerts_dba_info" {
   name = "sns_alerts_dba_info"
+  display_name = "RDS-Info"
 }
 
 resource "aws_sns_topic" "sns_alerts_dba_warning" {
   name = "sns_alerts_dba_warning"
+  display_name = "RDS-Warn"
 }
