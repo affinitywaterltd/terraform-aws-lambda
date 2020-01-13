abb9fd4361b1d2bb1836f4f16198c4bb3b22e728
Haddrell <Glenn.Haddrell@affinitywater.co.uk>
2020-01-13 16:27:41 +0000

    Update variables

--- daily_rds_check_free_space/variables.tf
+++ daily_rds_check_free_space/variables.tf
@@ -2,7 +2,10 @@
 variable "account" {}
 variable "environment" {}
 
-variable "sns_sms_list" {}
+variable "sns_sms_list_info" {}
+
+variable "sns_sms_list_warn" {}
+
 
 data "terraform_remote_state" "core" {
   backend = "atlas"
