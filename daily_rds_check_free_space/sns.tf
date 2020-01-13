5434a9a8c7aab11e6de0a731bcb1e9477cb4bf3d
Haddrell <Glenn.Haddrell@affinitywater.co.uk>
2020-01-13 16:23:25 +0000

    Remove provider

--- daily_rds_check_free_space/sns.tf
+++ daily_rds_check_free_space/sns.tf
@@ -12,7 +12,6 @@ resource "aws_sns_topic" "sns_alerts_dba_warning" {
 
 resource "aws_sns_topic_subscription" "sns-topic-info" {
   count     = "${length(var.sns_sms_list)}"
-  provider  = "aws.sns2sms"
   topic_arn = "${aws_sns_topic.sns_alerts_dba_info.arn}"
   protocol  = "sms"
   endpoint  = "${var.sns_sms_list.["info"]}"
