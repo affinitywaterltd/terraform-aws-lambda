4dda5d97f312c8061abf23c615fe848d38c057a4
Haddrell <Glenn.Haddrell@affinitywater.co.uk>
2020-08-04 08:10:21 +0100

    Updated Pagination

--- daily_aws_backup_expired_deletion/daily_aws_backup_expired_deletion.py
+++ daily_aws_backup_expired_deletion/daily_aws_backup_expired_deletion.py
@@ -6,8 +6,8 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 backup = boto3.client('backup')
-page_size = 100
-response = []
+page_size = 1000
+
 
 def lambda_handler(event, context):
 
@@ -20,15 +20,22 @@ def lambda_handler(event, context):
     # For each Vault
     for backupvault in backupVaultNames:
         logger.info("Checking Recover points in vault: " + backupvault)
+        recoverypoints = []
         while True:
-            if 'NextToken' in response:
+            if 'NextToken' in recoverypoints:
                 logger.info("Next page")
-                recoverypoints = backup.list_recovery_points_by_backup_vault(BackupVaultName=backupvault, MaxResults=int(page_size),NextToken=response['NextToken'])
+                recoverypoints = backup.list_recovery_points_by_backup_vault(BackupVaultName=backupvault, MaxResults=int(page_size),NextToken=recoverypoints['NextToken'])
             else:
-                logger.info("Next page")
+                logger.info("First page")
                 recoverypoints = backup.list_recovery_points_by_backup_vault(BackupVaultName=backupvault, MaxResults=int(page_size))
             
             for recoverypoint in recoverypoints['RecoveryPoints']:
                 if recoverypoint['Status'] == 'EXPIRED':
                     logger.info("Deleting expired recovery point: " + recoverypoint['RecoveryPointArn'])
                     result = backup.delete_recovery_point(BackupVaultName=backupvault,RecoveryPointArn=recoverypoint['RecoveryPointArn'])
+            logger.info("Completed batch")
+                
+            if 'NextToken' not in recoverypoints:
+                break
+                
+        logger.info("Completed vault")
