e5e042455fd64b72c7e006f6a56019e5c615d368
Haddrell <Glenn.Haddrell@affinitywater.co.uk>
2020-08-04 07:21:24 +0100

    Add logging ole

--- daily_aws_backup_expired_deletion/iam.tf
+++ daily_aws_backup_expired_deletion/iam.tf
@@ -23,3 +23,31 @@ resource "aws_iam_role_policy_attachment" "lambda_snapshot_cleaup_policy_attach"
   role       = aws_iam_role.lambda_aws_backup_cleanup_role.name
   policy_arn = "arn:aws:iam::aws:policy/AWSBackupFullAccess"
 }
+
+resource "aws_iam_policy" "lambda_cloudwatch_logs_writing_policy" {
+  name   = "lambda-cloudwatch-logs-writing"
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "VisualEditor0",
+            "Effect": "Allow",
+            "Action": [
+                "logs:DescribeLogGroups",
+                "logs:PutRetentionPolicy",
+                "logs:CreateLogGroup",
+                "logs:CreateLogStream",
+                "logs:PutLogEvents"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_backup_logging_policy_attach" {
+  role       = aws_iam_role.lambda_aws_backup_cleanup_role.name
+  policy_arn = aws_iam_policy.lambda_cloudwatch_logs_writing_policy.arn
+}
