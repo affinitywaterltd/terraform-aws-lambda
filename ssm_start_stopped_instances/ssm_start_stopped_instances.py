11b66070a16a14fac6e588da2e65be2fe2a5b78f
Glenn Haddrell <glenn.haddrell@affinitywater.co.uk>
2021-03-31 15:07:43 +0100

    Update lambda with waits

--- ssm_start_stopped_instances/ssm_start_stopped_instances.py
+++ ssm_start_stopped_instances/ssm_start_stopped_instances.py
@@ -1,10 +1,13 @@
 import json
 import boto3
+import time
+from random import randrange
 
 ec2 = boto3.client('ec2')
 
 def lambda_handler(event, context):
-    
+    # Perform random sleep to ensure that concurrent executions to not exceed the rate limit of the API
+    time.sleep(randrange(20))
     ssmMaintenanceWindow = event['ssmMaintenanceWindow']
     print("Checking for instances with ssmMaintenanceWindow tag value - {}".format(ssmMaintenanceWindow))
     
@@ -36,7 +39,6 @@ def lambda_handler(event, context):
                     InstanceIds = [
                         instanceId
                     ],
-                    DryRun = True
                 )
             
             
