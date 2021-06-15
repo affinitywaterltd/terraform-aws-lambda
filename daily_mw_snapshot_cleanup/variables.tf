65eb214c60056183009e0ac5bd32ec80c111cc55
Glenn Haddrell <glenn.haddrell@affinitywater.co.uk>
2021-06-15 15:47:13 +0100

    Updated variables.tf

--- daily_mw_snapshot_cleanup/variables.tf
+++ daily_mw_snapshot_cleanup/variables.tf
@@ -1,2 +1,13 @@
 variable "account" {}
 variable "environment" {}
+
+data "terraform_remote_state" "core" {
+  backend = "remote"
+
+  config = {
+    organization = "AffinityWater"
+    workspaces = {
+      name =  "${var.account}-core-${lookup(local.environment_map, var.environment, "prod")}"
+    }
+  }
+}
--- daily_rds_check_free_space/variables.tf
+++ daily_rds_check_free_space/variables.tf
@@ -8,3 +8,14 @@ variable "sns_sms_list_info" {
 variable "sns_sms_list_warn" {
   default = []
 }
+
+data "terraform_remote_state" "core" {
+  backend = "remote"
+
+  config = {
+    organization = "AffinityWater"
+    workspaces = {
+      name =  "${var.account}-core-${lookup(local.environment_map, var.environment, "prod")}"
+    }
+  }
+}
